358b31c1f8da3298ed02b720f20fb2f342b58448
Lev <levprimak1007@gmail.com>
2025-11-15 19:15:53 +0300

    feature(all): general things

--- Tg_bot/src/api.py
+++ Tg_bot/src/api.py
@@ -1,22 +1,17 @@
 import aiohttp
-import json
-
-
 async def make_request(text: str):
-    url = "http://ai_service:3000/ai/make_request"
+    url = "http://localhost:3000/ai/make_request"  # Измените на localhost
 
-    # Создаем JSON тело запроса вместо параметров URL
     payload = {"message": text}
 
     async with aiohttp.ClientSession() as session:
         async with session.post(
                 url,
-                json=payload,  # Используем json вместо params для передачи тела запроса
+                json=payload,
                 headers={"accept": "application/json", "Content-Type": "application/json"}
         ) as response:
             if response.status == 200:
                 data = await response.json()
-                # Предполагаем, что ответ имеет структуру AIResponse
                 return data.get("content", data)
             else:
                 error_text = await response.text()
